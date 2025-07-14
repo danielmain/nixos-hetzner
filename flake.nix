43e6b3427b33e785402f3232993642e04d7a1a67
Daniel Main <daniel@funktional.dev>
2025-07-14 17:51:42 +0200

    ExecStart is defined in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,16 +1,38 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    deploy-rs.url = "github:serokell/deploy-rs";
+    visitflow.url = "github:danielmain/visitflow-backend";
   };
-
-  outputs = { nixpkgs, ... }: {
+  
+  outputs = { nixpkgs, deploy-rs, visitflow, ... }: {
     nixosConfigurations = {
       hetzner-x86_64 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
+          {
+            # Use the app package from your repo
+            systemd.services.visitflow.serviceConfig.ExecStart = 
+              "${visitflow.packages.x86_64-linux.default}/bin/visitflow-backend";
+          }
         ];
       };
     };
+    
+    deploy.nodes.hetzner = {
+      hostname = "static.120.186.55.162.clients.your-server.de";
+      sshUser = "daniel";
+      user = "root";
+      
+      profiles.system = {
+        path = deploy-rs.lib.x86_64-linux.activate.nixos 
+          self.nixosConfigurations.hetzner-x86_64;
+      };
+    };
+    
+    checks = builtins.mapAttrs 
+      (system: deployLib: deployLib.deployChecks self.deploy) 
+      deploy-rs.lib;
   };
 }
